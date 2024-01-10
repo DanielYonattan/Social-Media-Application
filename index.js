1eaa028a80feddeb4cd8522334a35c09b6399a6e
Daniel <danielyonattan@gmail.com>
2024-01-10 12:34:26 -0500

    init

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL); 
+
+// middleware
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+  
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 
 
-app.listen(8000, () => {
+    
+app.listen(3000, () => {
     console.log("server is running");
-}) 
+})       
