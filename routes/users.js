c10e920b58d8d424c10a7648f701a730216444c9
Daniel <danielyonattan@gmail.com>
2024-01-11 14:28:34 -0500

    Add delete user functionality

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 
 // update bio
-router.put("/:id", async (req, res) => {
+router.put("/update/:id", async (req, res) => {
     if (req.body.userId != req.params.id)
         res.status(403).json("cannot update an account you don't own");
     
@@ -25,6 +25,21 @@ router.put("/:id", async (req, res) => {
 
  
 // delete
+router.delete("/delete/:id", async (req, res) => {
+    if (req.body.userId != req.params.id)
+        res.status(403).json("cannot modify accout you do not own");
+
+    try {
+        user = await User.deleteOne({
+            _id: req.body.userId
+        })
+
+        res.status(200).json(user);
+    }
+    catch(err){ 
+        res.status(500).json(err);
+    }
+})
 
 // get
 
