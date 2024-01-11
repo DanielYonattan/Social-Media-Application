be896aed5acf399787250b6aa8e43de19eb6c46f
Daniel <danielyonattan@gmail.com>
2024-01-11 14:45:32 -0500

    Add get user functionality

--- routes/users.js
+++ routes/users.js
@@ -42,6 +42,19 @@ router.delete("/delete/:id", async (req, res) => {
 })
 
 // get
+router.get("/get/:id", async (req, res) => {
+    try {
+        user = await User.findOne({
+            _id: req.params.id
+        })
+        const {password, updatedAt, ...other} = user._doc;
+
+        res.status(200).json(other);
+    }
+    catch(err){ 
+        res.status(500).json(err);
+    }
+})
 
 // follow
 
