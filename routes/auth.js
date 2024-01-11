170fca35fd6f2a92220a2c58f70078d9c065cf4a
Daniel <danielyonattan@gmail.com>
2024-01-11 13:52:22 -0500

    Add register and login functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,42 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-    res.send("auth");
+
+// Register
+router.post("/register", async (req, res) => {
+    try { 
+        const user = await new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        await user.save();
+        res.status(200).json(user);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
+
+// login
+router.post("/login", async (req,res) => {
+    try {
+        const user = await User.findOne({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        if (user == null)
+            res.status(404).json("user not found");
+        else
+            res.status(200).json(user);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
 })
 
 module.exports = router;
 
+   
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-    res.send("user route");
+// Register
+router.post("/", async (req, res) => {
+    const user = await new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    await user.save();
 })
 
 module.exports = router;
